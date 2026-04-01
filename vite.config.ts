35a2e1a2f5b21e6cd31e5d10297fde6012264d52
张江 <zhangjiang@zhangjiangdeMacBook-Pro.local>
2026-04-01 10:35:21 +0800

    修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -65,19 +65,21 @@ const viteConfig = defineConfig((mode: ConfigEnv) => {
 			proxy: {
 				'/api/gen': {
 					//单体架构下特殊处理代码生成模块代理
-					target: env.VITE_IS_MICRO === 'true' ? env.VITE_ADMIN_PROXY_PATH : env.VITE_GEN_PROXY_PATH,
+					// target: env.VITE_IS_MICRO === 'true' ? env.VITE_ADMIN_PROXY_PATH : env.VITE_GEN_PROXY_PATH,
+					target: env.VITE_ADMIN_PROXY_PATH,
 					changeOrigin: true,
 					rewrite: (path) => path.replace(/^\/api/, ''),
 				},
 				'/ai/smart': {
-					target: isDev ? env.VITE_SMART_ANNOTATE_PROXY_PATH : env.VITE_SMART_ANNOTATE_PROXY_PATH, // 目标服务器地址
+					// target: isDev ? env.VITE_SMART_ANNOTATE_PROXY_PATH : env.VITE_SMART_ANNOTATE_PROXY_PATH, // 目标服务器地址
+					target: env.VITE_SMART_ANNOTATE_PROXY_PATH, // 目标服务器地址
 					ws: false, // 是否启用 WebSocket
 					changeOrigin: true, // 是否修改请求头中的 Origin 字段
 					rewrite: (path) => path.replace(/^\/ai\/smart/, ''),
 				},
 				'/api': {
-					target: isDev ? env.VITE_ADMIN_PROXY_PATH : env.VITE_API_URL, // 目标服务器地址
-					// target: env.VITE_ADMIN_PROXY_PATH, // 目标服务器地址
+					// target: isDev ? env.VITE_ADMIN_PROXY_PATH : env.VITE_API_URL, // 目标服务器地址
+					target: env.VITE_ADMIN_PROXY_PATH, // 目标服务器地址
 					ws: false, // 是否启用 WebSocket
 					changeOrigin: true, // 是否修改请求头中的 Origin 字段
 					rewrite: (path) => path.replace(/^\/api/, ''),
